8b37f3b3b60577c7adb3891a539a0a3208909de9
johnrobert7991 <134179158+johnrobert7991@users.noreply.github.com>
2024-10-13 16:37:52 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,77 @@
+import logging
+import os
+import signal
+import sys
+from datetime import timedelta
+from neonize.client import NewClient
+from neonize.events import (
+    ConnectedEv,
+    MessageEv,
+    PairStatusEv,
+    event,
+    ReceiptEv,
+    CallOfferEv,
+    QREv
+)
+from neonize.proto.Neonize_pb2 import JID
+from neonize.proto.waE2E.WAWebProtobufsE2E_pb2 import (
+    Message,
+    FutureProofMessage,
+    InteractiveMessage,
+    MessageContextInfo,
+    DeviceListMetadata,
+)
+from neonize.types import MessageServerID
+from neonize.utils import log
+from neonize.utils.enum import ReceiptType
+from neonize import Event
+
+
+def interrupted(*_): 
+    event.set()
+
+
+log.setLevel(logging.CRITICAL)
+signal.signal(signal.SIGINT, interrupted)
+
+client = NewClient("db.sqlite3")
+
+@client.event(ConnectedEv)
+def on_connected(_: NewClient, __: ConnectedEv):
+    log.info("⚡ Connected")
+
+
+@client.event(ReceiptEv)
+def on_receipt(_: NewClient, receipt: ReceiptEv):
+    log.debug(receipt)
+
+
+@client.event(CallOfferEv)
+def on_call(_: NewClient, call: CallOfferEv):
+    log.debug(call)
+
+
+@client.event(MessageEv)
+def on_message(client: NewClient, message: MessageEv):
+    handler(client, message)
+
+
+def handler(client: NewClient, message: MessageEv):
+    text = message.Message.conversation or message.Message.extendedTextMessage.text
+    chat = message.Info.MessageSource.Chat
+    print(message.Message)
+    match text:
+        case "ping":
+            client.reply_message("pong", message)
+
+
+@client.event(PairStatusEv)
+def PairStatusMessage(_: NewClient, message: PairStatusEv):
+    log.info(f"logged as {message.ID.User}")     
+
+
+if __name__ == "__main__": 
+    code = client.PairPhone("6285155304081", show_push_notification=False)
+    print(code)
+    
+
